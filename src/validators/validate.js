75b012fc72468afc0d0711dfc3f541a5c2ab69b0
AlexandrKoliukh <alexkoliukh@gmial.com>
2019-09-29 18:16:42 +0300

    validate fix

--- src/validators/validate.js
+++ src/validators/validate.js
@@ -1,15 +1,19 @@
 const validate = values => {
   const errors = {};
   if (!values.name) {
-
+    errors.name = 'Required';
   } else if (values.name.length > 15) {
-    errors.name = 'Must be 15 characters or less'
+    errors.name = 'Must be 15 characters or less';
   }
-  if (!/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i.test(values.ip)) {
-    errors.ip = 'Invalid ip address'
+  if (!values.ip) {
+    errors.ip = 'Required';
+  } else if (!/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i.test(values.ip)) {
+    errors.ip = 'Invalid ip address';
   }
-  if (values.port < 1 || values.port > 65535) {
-    errors.port = 'Must be in [1; 65 535]'
+  if (!values.port) {
+    errors.port = 'Required';
+  } else if (values.port < 1 || values.port > 65535) {
+    errors.port = 'Must be in [1; 65 535]';
   }
 
   return errors;
