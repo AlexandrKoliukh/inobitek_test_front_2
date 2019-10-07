8e8652e85d339a9d0615eea4799f3c73c916627a
AlexandrKoliukh <alexkoliukh@gmail.com>
2019-10-07 14:43:59 +0300

    Updates

---
+++ precache-manifest.0368d7f210c024cc2041cb19cc1101a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a152363bd12cf72fa461689f0568fc5b",
+    "url": "/inobitek_test_front_2/index.html"
+  },
+  {
+    "revision": "be2d972610d8ce91a6ed",
+    "url": "/inobitek_test_front_2/static/css/2.11b2e342.chunk.css"
+  },
+  {
+    "revision": "cafba87240eaf32b63d3",
+    "url": "/inobitek_test_front_2/static/css/main.16b34046.chunk.css"
+  },
+  {
+    "revision": "be2d972610d8ce91a6ed",
+    "url": "/inobitek_test_front_2/static/js/2.601a1ab7.chunk.js"
+  },
+  {
+    "revision": "cafba87240eaf32b63d3",
+    "url": "/inobitek_test_front_2/static/js/main.1de782c0.chunk.js"
+  },
+  {
+    "revision": "c833768db646aa1edda5",
+    "url": "/inobitek_test_front_2/static/js/runtime-main.1589aace.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/inobitek_test_front_2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/inobitek_test_front_2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/inobitek_test_front_2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/inobitek_test_front_2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/inobitek_test_front_2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
